1af144e15e627716c3c590b17188b2e37bc2476b
soohyuneeee <soohyuneeee12@gmail.com>
2023-08-03 13:39:53 +0900

    {Add} init

---
+++ src/main/java/com/moongpull/ddobagi/DdobagiApplication.java
@@ -0,0 +1,13 @@
+package com.moongpull.ddobagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdobagiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdobagiApplication.class, args);
+	}
+
+}
